7dbddb856355da2be83ec427c2d437698abb23fe
Surendran Sukumaran <surendrans86@gmail.com>
2016-06-06 12:44:42 +0530

    Removed comments unwanted

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,8 +1,7 @@
 class EmployeesController < ApplicationController
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
 
-  # GET /employees
-  # GET /employees.json
+
   def index
     @employees = Employee.all
   end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ActiveRecord::Base
   #add relation
+  #has_many
 end
