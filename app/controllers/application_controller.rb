77e2c7b8e22492565612eace8fabc415f78b73d4
Surendran Sukumaran <surendrans86@gmail.com>
2016-07-05 18:46:41 +0530

    Add comments to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   #addinsasda ds
   #  For APIs, you may want to use :null_session instead.
-  #  #add comments
+  #  #add comments 1
   protect_from_forgery with: :exception
 end
